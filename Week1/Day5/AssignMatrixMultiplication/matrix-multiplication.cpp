61255e63ab5d9721661d8e714dbe78b5dc7f025b
Ubuntu <ubuntu@ip-172-31-54-115.us-west-2.compute.internal>
2021-12-11 22:52:13 +0000

    Added annotations. Expanded examples

--- Week1/Day5/AssignMatrixMultiplication/matrix-multiplication.cpp
+++ Week1/Day5/AssignMatrixMultiplication/matrix-multiplication.cpp
@@ -43,10 +43,23 @@
 
 int main(void)
 {
-	int arr1[3][2] = { {1, 2}, {3, 4}, {5, 6} };
-	int arr2[2][3] = { {5, 6, 7}, {7, 8, 9} };
-	int arr3[1][4] = { {1, 2, 3, 4} };
-	int arr4[4][1] =
+	// Let's create some templates for our matrices using int arrays! //
+	int arr1[3][2] = // 3x2
+	{
+		{1, 2},
+		{3, 4},
+		{5, 6}
+	};
+	int arr2[2][3] = // 2x3
+	{
+		{5, 6, 7}, 
+		{7, 8, 9}
+	};
+	int arr3[1][4] = // 1x4
+	{
+		{1, 2, 3, 4}
+	};
+	int arr4[4][1] = // 4x1
 	{ 
 		{1,}, 
 		{5,}, 
@@ -54,36 +67,48 @@ int main(void)
 		{13,},
 	};
 
-	Matrix a(arr1);
+	/* Multiplication */
+	std::cout << "Matrix A:" << std::endl;
+	Matrix a(arr1); // We instantiate a matrix using arrays!
 	a.print();
+	std::cout << "Matrix B:" << std::endl;
 	Matrix b(arr2);
 	b.print();
-
+	
+	std::cout << "Matrix A x B:" << std::endl;
 	Matrix c = a.dot_product(b);
 	c.print();
 
-	// This should error
+	// This should error because we can only multiply two matrices of similar height and width.
 	//Matrix m1(arr3);
 	//Matrix err = a.dot_product(m1); // Assert fails, matrix a's rows not equal to matrix m1's cols.
 	
 	std::cout << "\n\n\n\n\n\n"; // Just flushing the buffer a bit...
 
+	// Let's make Matrix D using the values in arr3.
+	std::cout << "Matrix D:" << std::endl;
 	Matrix d(arr3);
 	d.print();
+	// Matrix e using the values in arr4...
+	std::cout << "Matrix E:" << std::endl;
 	Matrix e(arr4);
 	e.print();
+	std::cout << "Matrix D x E:" << std::endl;
 	Matrix dot_de = d.dot_product(e);
 
 	dot_de.print();
-
+	
+	// Final example:
+	// 	Let's square a matrix (Full disclosure: I have no idea what squaring a matrix actually is or if it's a real concept)
 	int arrA[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
 	int arrB[3][3];
 	memcpy(&arrB, &arrA, sizeof(int) * 3 * 3);
 	Matrix matA(arrA);
 	Matrix matB(arrB);
+	std::cout << "Matrix to Square:" << std::endl;
 	matA.print();
-	matB.print();
 	
+	std::cout << "Matrix Squared:" << std::endl;
 	Matrix dotAB = matA.dot_product(matB);
 	dotAB.print();
 
